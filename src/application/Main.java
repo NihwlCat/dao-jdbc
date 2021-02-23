64a0d5f922ec2c9ba0d2df01f2389c2629e104fa
NihwlCat <pedro.ra.nihwl@gmail.com>
2021-02-23 19:42:06 -0300

    class Seller created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,16 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21,"Bob","bob@gmail",new Date(),2500.00,obj);
+
+        System.out.println(seller);
     }
 }
---
+++ src/entities/Seller.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    private Department department;
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seller seller = (Seller) o;
+
+        return id != null ? id.equals(seller.id) : seller.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department + '}';
+    }
+}
